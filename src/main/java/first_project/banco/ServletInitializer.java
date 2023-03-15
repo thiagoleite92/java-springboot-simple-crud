1003dd52ae334932911ac1bb0dd8381e26ceccdc
Thiago Leite <leite.tjs@gmail.com>
2023-03-15 17:15:18 -0300

    Initial commit

---
+++ src/main/java/first_project/banco/ServletInitializer.java
@@ -0,0 +1,14 @@
+package first_project.banco;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+  @Override
+  protected SpringApplicationBuilder configure(
+    SpringApplicationBuilder application
+  ) {
+    return application.sources(BancoApplication.class);
+  }
+}
---
+++ src/main/java/first_project/banco/controller/HelloController.java
@@ -0,0 +1,13 @@
+package first_project.banco.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/health")
+    public String index() {
+        return "Rafael boiola";
+    }
+}
